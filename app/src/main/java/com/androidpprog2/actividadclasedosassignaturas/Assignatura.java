a6d54f617ad5f873f27def4a64cb7deab6e51229
Oscar Romero Doblas <oscar.romero@students.salle.url.edu>
2022-03-05 19:51:08 +0100

    bject Assignatura (DONE)

--- app/src/main/java/com/androidpprog2/actividadclasedosassignaturas/Assignatura.java
+++ app/src/main/java/com/androidpprog2/actividadclasedosassignaturas/Assignatura.java
@@ -2,5 +2,48 @@ package com.androidpprog2.actividadclasedosassignaturas;
 
 public class Assignatura {
 
-    
+    int id;
+    String nombreAssignatura;
+    String descripcion;
+    String img;
+
+    public Assignatura(int id, String nombreAssignatura, String descripcion, String img) {
+        this.id = id;
+        this.nombreAssignatura = nombreAssignatura;
+        this.descripcion = descripcion;
+        this.img = img;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombreAssignatura() {
+        return nombreAssignatura;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombreAssignatura(String nombreAssignatura) {
+        this.nombreAssignatura = nombreAssignatura;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
 }
